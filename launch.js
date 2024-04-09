5f9cf9d92da41da2c6f6940094197190b0043709
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-04-09 18:19:04 +0100

    Update launch.js

--- launch.js
+++ launch.js
@@ -4,17 +4,15 @@ for (let i = 0; i < playersInTabList.length; i++) {
     let players = playersInTabList[i].getName();
     let uuids = playersInTabList[i].getUUID();
 
-    if (uuids[14] === "4") {
+    if (uuids[14] === 4) {
         Chat.log("§a" + players + " §fwith " + "§a" + uuids);
-    } else {
-        Chat.log("§4" + players + " §fwith " + "§4" + uuids);
+        FS.open("validPlayers.txt").append(players + "\r\n");
+        FS.open("validUUIDs.txt").append(uuids + "\r\n");
     }
 
-    FS.createFile("", "players.txt");
-    let playersFile = FS.open("players.txt");
-    playersFile.append(players + "\r\n");
-
-    FS.createFile("", "uuids.txt");
-    let uuidsFile = FS.open("uuids.txt");
-    uuidsFile.append(uuids + "\r\n");
+    if (uuids[14] !== 4) {
+        Chat.log("§4" + players + " §fwith " + "§4" + uuids);
+        FS.open("invalidPlayers.txt").append(players + "\r\n");
+        FS.open("invalidUUIDs.txt").append(uuids + "\r\n");
+    }
 }
