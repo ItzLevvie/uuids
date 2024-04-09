52fbf35f8b74989d08d3ec56807bbb8566491a9a
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-04-09 17:00:24 +0100

    Update launch.js

--- launch.js
+++ launch.js
@@ -3,7 +3,7 @@ let playersInTabList = World.getPlayers();
 for (let i = 0; i < playersInTabList.length; i++) {
     let players = playersInTabList[i].getName();
     let uuids = playersInTabList[i].getUUID();
-    Chat.log("§a" + players + " = " + uuids);
+    Chat.log("§a" + players + " §fwith " + "§a" + uuids);
 
     FS.createFile("", "players.txt");
     let playersFile = FS.open("players.txt");
