afe5a99eeafd6bc0a93899f341a475c0ec5646df
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-04-11 15:03:04 +0100

    Update launch.js

--- launch.js
+++ launch.js
@@ -6,21 +6,36 @@ FS.createFile("", "validUUIDs.txt");
 FS.createFile("", "invalidPlayers.txt");
 FS.createFile("", "invalidUUIDs.txt");
 
+FS.createFile("", "DEBUG.txt");
+
 for (let i = 0; i < playersInTabList.length; i++) {
     let players = playersInTabList[i].getName();
     let uuids = playersInTabList[i].getUUID();
 
     if (players !== "" && !players.startsWith("!") && players !== "Carpenter ") {
+
+        // Minecraft puts all players under UUID version 4.
         if (uuids[14] === "4") {
             Chat.log("§a" + players + " §fwith " + "§a" + uuids);
             FS.open("validPlayers.txt").append(players + "\r\n");
             FS.open("validUUIDs.txt").append(uuids + "\r\n");
+            FS.open("DEBUG.txt").append(players + " with " + uuids + " (UUID version 4) " + "\r\n");
         }
 
-        if (uuids[14] !== "4") {
+        // Hypixel puts all NPCs under UUID version 2.
+        if (uuids[14] === "2") {
             Chat.log("§4" + players + " §fwith " + "§4" + uuids);
             FS.open("invalidPlayers.txt").append(players + "\r\n");
             FS.open("invalidUUIDs.txt").append(uuids + "\r\n");
+            FS.open("DEBUG.txt").append(players + " (NPC) with " + uuids + " (UUID version 2) " + "\r\n");
+        }
+
+        // Hypixel puts all nicked players under UUID version 1.
+        if (uuids[14] === "1") {
+            Chat.log("§4" + players + " §fwith " + "§4" + uuids);
+            FS.open("invalidPlayers.txt").append(players + "\r\n");
+            FS.open("invalidUUIDs.txt").append(uuids + "\r\n");
+            FS.open("DEBUG.txt").append(players + " (nicked player) with " + uuids + " (UUID version 1) " + "\r\n");
         }
     }
 }
