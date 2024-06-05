9cf35aba369f2e36d85c7c1c2102ce475fa74867
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-06-05 21:28:21 +0100

    Update launch.js

--- launch.js
+++ launch.js
@@ -1,5 +1,3 @@
-var playersInTabList = World.getPlayers();
-
 FS.createFile("", "validPlayers.txt");
 FS.createFile("", "validUUIDs.txt");
 
@@ -8,14 +6,16 @@ FS.createFile("", "invalidUUIDs.txt");
 
 FS.createFile("", "DEBUG.txt");
 
-var players = [];
-var NPCs = [];
-var nickedPlayers = [];
-var bedrockPlayers = [];
+var onlinePlayers = World.getPlayers();
 
-for (var i = 0; i < playersInTabList.length; i++) {
-    var player = playersInTabList[i].getName();
-    var uuid = playersInTabList[i].getUUID();
+var javaEditionPlayers = [];
+var hypixelNPCs = [];
+var hypixelNickedPlayers = [];
+var bedrockEditionPlayers = [];
+
+for (var i = 0; i < onlinePlayers.length; i++) {
+    var player = onlinePlayers[i].getName();
+    var uuid = onlinePlayers[i].getUUID();
     var uuidVersion = uuid[14];
 
     // Use the "^[a-zA-Z0-9_]+$" regular expression to filter out bad usernames
@@ -23,7 +23,7 @@ for (var i = 0; i < playersInTabList.length; i++) {
 
         // Minecraft puts all Java Edition players under UUID version 4 (variant 1).
         if (uuidVersion === "4") {
-            players.push(player);
+            javaEditionPlayers.push(player);
             FS.open("validPlayers.txt").append(player + "\r\n");
             FS.open("validUUIDs.txt").append(uuid + "\r\n");
             FS.open("DEBUG.txt").append(player + " (Java Edition player) with " + uuid + " (UUID version 4)" + "\r\n");
@@ -34,7 +34,7 @@ for (var i = 0; i < playersInTabList.length; i++) {
             if (player === "Carpenter ") {
                 player = "Carpenter";
             }
-            NPCs.push(player);
+            hypixelNPCs.push(player);
             FS.open("invalidPlayers.txt").append(player + "\r\n");
             FS.open("invalidUUIDs.txt").append(uuid + "\r\n");
             FS.open("DEBUG.txt").append(player + " (NPC) with " + uuid + " (UUID version 2)" + "\r\n");
@@ -42,7 +42,7 @@ for (var i = 0; i < playersInTabList.length; i++) {
 
         // Servers like Hypixel puts all nicked players under UUID version 1.
         if (uuidVersion === "1") {
-            nickedPlayers.push(player);
+            hypixelNickedPlayers.push(player);
             FS.open("invalidPlayers.txt").append(player + "\r\n");
             FS.open("invalidUUIDs.txt").append(uuid + "\r\n");
             FS.open("DEBUG.txt").append(player + " (nicked player) with " + uuid + " (UUID version 1)" + "\r\n");
@@ -50,35 +50,37 @@ for (var i = 0; i < playersInTabList.length; i++) {
 
         // Servers like WildNetwork puts all Bedrock Edition players under UUID version 0.
         if (uuidVersion === "0") {
-            bedrockPlayers.push(player);
+            bedrockEditionPlayers.push(player);
             FS.open("invalidPlayers.txt").append(player + "\r\n");
             FS.open("invalidUUIDs.txt").append(uuid + "\r\n");
             FS.open("DEBUG.txt").append(player + " (Bedrock Edition player) with " + uuid + " (UUID version 0)" + "\r\n");
         }
+
     }
 }
 
-var totalPlayers = players.length + nickedPlayers.length + bedrockPlayers.length;
+var javaEditionPlayerCount = javaEditionPlayers.length;
+var hypixelNPCsCount = hypixelNPCs.length;
+var hypixelNickedPlayerCount = hypixelNickedPlayers.length;
+var bedrockEditionPlayerCount = bedrockEditionPlayers.length;
+
+var totalPlayers = javaEditionPlayerCount + hypixelNickedPlayerCount + bedrockEditionPlayerCount;
+
+var javaEditionPlayersInAlphabetical = javaEditionPlayers.sort();
+var hypixelNPCsInAlphabetical = hypixelNPCs.sort();
+var bedrockEditionPlayersInAlphabetical = bedrockEditionPlayers.sort();
+var hypixelNickedPlayersInAlphabetical = hypixelNickedPlayers.sort();
+
 if (totalPlayers > 0) {
     Chat.log("§dOnline players: " + totalPlayers);
-}
-
-var sortedPlayers = players.sort();
-if (players.length > 0) {
-    Chat.log("§aList of Java Edition players (" + players.length + "): " + sortedPlayers.toString().replaceAll(",", "§f,§a ").replaceAll("§r", ""));
-}
-
-var sortedBedrockPlayers = bedrockPlayers.sort();
-if (sortedBedrockPlayers.length > 0) {
-    Chat.log("§eList of Bedrock Edition players (" + bedrockPlayers.length + "): " + bedrockPlayers.toString().replaceAll(",", "§f,§e ").replaceAll("§r", ""));
-}
-
-var sortedNPCs = NPCs.sort();
-if (NPCs.length > 0) {
-    Chat.log("§8List of NPCs (" + NPCs.length + "): " + sortedNPCs.toString().replaceAll(",", "§f,§8 ").replaceAll("§r", ""));
-}
-
-var sortedNickedPlayers = nickedPlayers.sort();
-if (nickedPlayers.length > 0) {
-    Chat.log("§4List of nicked players (" + nickedPlayers.length + "): " + sortedNickedPlayers.toString().replaceAll(",", "§f,§4 ").replaceAll("§r", ""));
+    Chat.log("§aList of Java Edition players (" + javaEditionPlayerCount + "): " + javaEditionPlayersInAlphabetical.toString().replaceAll(",", "§f,§a ").replaceAll("§r", ""));
+    if (hypixelNPCsCount.length > 0) {
+        Chat.log("§8List of NPCs (" + hypixelNPCsCount + "): " + hypixelNPCsInAlphabetical.toString().replaceAll(",", "§f,§8 ").replaceAll("§r", ""));
+    }
+    if (hypixelNickedPlayerCount > 0) {
+        Chat.log("§4List of nicked players (" + hypixelNickedPlayerCount + "): " + hypixelNickedPlayersInAlphabetical.toString().replaceAll(",", "§f,§4 ").replaceAll("§r", ""));
+    }
+    if (bedrockEditionPlayerCount.length > 0) {
+        Chat.log("§eList of Bedrock Edition players (" + bedrockEditionPlayerCount + "): " + bedrockEditionPlayersInAlphabetical.toString().replaceAll(",", "§f,§e ").replaceAll("§r", ""));
+    }
 }
